7a87befe26748ac76d333a8d58257cf09f6abfc6
Brandon Cochrane <bcoch346@mtroyal.ca>
2018-03-15 16:54:08 -0600

    update home page

--- index.js
+++ index.js
@@ -32,14 +32,23 @@ var app = express();
 app.use(parser.json());
 app.use(parser.urlencoded({ extended: true }));
 
-
+//home page view
 app.get('/', function(req, res) {
     res.writeHead(200, { "Content-Type": "text/html" });
-    res.write("Hello this is our COMP 4513 Assignment 2\n");
+    res.write("<h1>COMP 4513 Assignment 2</h1>");
+    res.write("<div><h2>Contributors</h2><ul><li>George Chase</li><li>Brandon Cochrane</li><li>Jorge Castano Dominguez<li><li>Catie Vickers</li></ul></div>");
+    res.write("<div>");
     res.write("<h1>Test Links:</h1> \n");
-    res.write("<a href='/api/company/all'>/api/company/all</a>");
+    res.write("<ul>");
+    //res.write("<li><a href=''> </a></li>");
+    res.write("<li><a href='/api/company/all'>/api/company/all</a></li>");
+    res.write("<li><a href='/api/stocks/AMZN'>/api/stocks/:symbol => symbol = AMZN </a></li>");
+    res.write("</ul>");
+    res.write("</div>");
     res.end();
 });
+
+
 // AMZN stock route route.
 app.get('/api/company/all', function(req, res) {
     Company.find({}, function(err, data) {
