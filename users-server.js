dd1f73c2eea02ab39f950b045fe73c424cedcd14
Jorge <jcast236@mtroyal.ca>
2018-03-30 01:03:27 +0000

    modify post address

--- users-server.js
+++ users-server.js
@@ -16,7 +16,6 @@ module.exports = function(app, User) {
         
     //authentication sending back id, first, last if correct (question a.) is working
     app.post('/api/user', function (req, resp) {
-            console.log(req.body);
             User.find({email: req.body.email}, 'salt -_id', function(err, data) {
                 if(err) {
                     resp.json({ message: 'error!' });
@@ -25,14 +24,11 @@ module.exports = function(app, User) {
                     resp.json({ message: 'Email does not exist!'});
                 }
                 else {
-                    console.log("found user")
                     //user exists and salt and password combined and hashed
                     var userSalt = data[0]['salt'];
-                    console.log(req.body.password + " " + userSalt)
                     var saltAndPass = md5(req.body.password + userSalt, "hex");
-                    console.log(saltAndPass);
                     //match the value to password in user collection
-                    User.find({email: req.body.email, password: saltAndPass}, 'id first_name last_name -_id', function(err, match) {
+                    User.find({email: req.body.email, password: saltAndPass}, 'id first_name email last_name -_id', function(err, match) {
                         if(err) {
                             resp.json({ message: 'error!'});
                         }
@@ -40,7 +36,6 @@ module.exports = function(app, User) {
                             resp.json({ message: 'Password does not match! Authentication failed!'});
                         }
                         else {
-                            console.log("found passwd")
                             resp.json(match);
                         }
                     })
